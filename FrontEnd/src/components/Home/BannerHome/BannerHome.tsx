5ef2d0d8fece203188208069c8370911b6d9cf01
vitorvhsilva <dndragonbr@gmail.com>
2024-09-01 16:36:36 -0300

    Responsividade do BannerHome
    Co-authored-by: Joaogananca26 <Joaogananca26@users.noreply.github.com>
    Co-authored-by: lucasbraccoyamamoto@gmail.com <lucasbraccoyamamoto@gmail.com>

--- FrontEnd/src/components/Home/BannerHome/BannerHome.tsx
+++ FrontEnd/src/components/Home/BannerHome/BannerHome.tsx
@@ -1,12 +1,12 @@
 export default function BannerHome() {
   return (
-    <section className="w-full h-[80vh] bg-corFundoCinza flex justify-around items-center">
-      <div className="w-[35%] h-fit">
-        <h1 className="font-poppins text-5xl">Entenda o que está acontecendo com <span className="text-cor2">seu carro</span></h1>
-        <h2 className="font-poppins text-xl">Com a nossa I.A especializada em diagnósticos para veículos, se <span className="text-cor3">conectar</span> com o seu automóvel nunca foi tão facil</h2>
-        <button className="bg-cor1 p-2 rounded-xl text-corBranco mt-5 hover:bg-cor2 transition duration-500 hover:scale-110">Faça um Diagnóstico</button>
+    <section className="w-full h-[80vh] bg-corFundoCinza flex justify-around items-center lg:flex-row flex-col">
+      <div className="w-[60%] lg:w-[35%] h-fit">
+        <h1 className="font-poppins text-3xl lg:text-5xl">Entenda o que está acontecendo com <span className="text-cor2">seu carro</span></h1>
+        <h2 className="font-poppins text-md lg:text-xl">Com a nossa I.A especializada em diagnósticos para veículos, se <span className="text-cor3">conectar</span> com o seu automóvel nunca foi tão facil</h2>
+        <button className="w-44 h-12 bg-cor1 p-2 rounded-xl text-corBranco mt-5 hover:bg-cor2 transition duration-500 hover:scale-110 mx-auto">Faça um Diagnóstico</button>
       </div>
-      <div className="w-[40%] h-[60vh]">
+      <div className="w-[60%] lg:w-[40%] h-fit">
         <img src='./imgs/fotos/mecanico1.jpg' alt="Homem mexendo em um motor de carro" className="w-full h-full rounded-xl hover:scale-110 duration-700 transition hover:shadow-cor1 shadow-xl"/>
       </div>
     </section>
