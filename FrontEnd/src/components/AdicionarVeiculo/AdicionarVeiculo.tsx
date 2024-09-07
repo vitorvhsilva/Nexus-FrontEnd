86b1079f6054fb305ed96ada4f7657919d6c5777
joaogananca26 <joaogananca26@gmail.com>
2024-09-07 17:23:26 -0300

    Página de adicão de veículo concluida

--- FrontEnd/src/components/AdicionarVeiculo/AdicionarVeiculo.tsx
+++ FrontEnd/src/components/AdicionarVeiculo/AdicionarVeiculo.tsx
@@ -7,11 +7,27 @@ export default function AdicionarVeiculo(){
             <form action="" className="w-full h-fit px-40 mt-20">
                 <InputTextAdicionarVeiculo label="Marca" placeholder="Insira a marca do seu veículo" id="marca"/>
                 <InputTextAdicionarVeiculo label="Modelo" placeholder="Insira o modelo do seu veículo" id="modelo"/>
+                <label className="block text-2xl mb-5">Tipo do veículo</label>
+                <div className="w-full h-fit flex justify-between items-center">
+                    <div className="w-fit h-fit flex justify-center items-center gap-4">
+                        <input className="w-8 h-8" type="radio" id="html" name="fav_language" value="HTML"/>
+                        <label htmlFor="html" className="text-2xl">Carro</label>
+                    </div>
+                    <div className="w-fit h-fit flex justify-center items-center gap-4">
+                        <input className="w-8 h-8" type="radio" id="html" name="fav_language" value="HTML"/>
+                        <label htmlFor="html" className="text-2xl">Moto</label>
+                    </div>
+                    <div className="w-fit h-fit flex justify-center items-center gap-4">
+                        <input className="w-8 h-8" type="radio" id="html" name="fav_language" value="HTML"/>
+                        <label htmlFor="html" className="text-2xl">Caminhão</label>
+                    </div>
+                </div>
                 <div className="w-full h-fit flex flex-col gap-2 my-10">
                     <label htmlFor="ano" className="text-2xl">Ano</label>
                     <input type="number" id="ano" name="ano" placeholder="0" className="border-2 border-corPreto rounded-lg text-2xl w-28 p-2"/>
                 </div>
                 <InputTextAdicionarVeiculo label="Placa" placeholder="Insira a placa do seu veículo" id="placa"/>
+                <input type="submit" value="Cadastrar veículo" className="text-3xl bg-cor5 py-3 px-10 text-corBranco rounded-lg mx-auto block my-10"/>
             </form>
         </section>
     )
