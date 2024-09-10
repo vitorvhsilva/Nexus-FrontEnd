766cd269d35f281c39ea842145ded1eec22b0184
yamamotolucas <lucasbraccoyamamoto@gmail.com>
2024-09-10 13:18:09 -0300

    Adicionando componentes na Rota /user

--- FrontEnd/src/routes/PaginaUsuario/index.tsx
+++ FrontEnd/src/routes/PaginaUsuario/index.tsx
@@ -1,6 +1,13 @@
+import Cabecalho from "../../components/Cabecalho/Cabecalho";
+import PaginaUsuarioComponent from "../../components/PaginaUsuario/PaginaUsuarioComponent";
+import Rodape from "../../components/Rodape/Rodape";
+
 export default function PaginaUsuario() {
   return (
     <>
+    <Cabecalho/>
+    <PaginaUsuarioComponent/>
+    <Rodape/>
     </>
   )
 }
