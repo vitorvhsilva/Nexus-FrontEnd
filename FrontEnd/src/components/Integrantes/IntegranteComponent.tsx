ce9d4517a25c9476820708809451d779ecc76475
yamamotolucas <lucasbraccoyamamoto@gmail.com>
2024-09-09 22:08:19 -0300

    Fazendo página de Integrantes e ajustando responsividade

--- FrontEnd/src/components/Integrantes/IntegranteComponent.tsx
+++ FrontEnd/src/components/Integrantes/IntegranteComponent.tsx
@@ -4,12 +4,12 @@ export default function IntegranteComponent({imgIntegrante, nomeIntegrante, rmIn
     return(
         <>
         <div className="font-poppins">
-            <div className="flex flex-col items-center">
-                <img src={imgIntegrante} alt={`Foto de ${nomeIntegrante}`}  className="w-60 h-60 rounded-full mb-5"></img>
+            <div className="flex flex-col items-center m-5">
+                <img src={imgIntegrante} alt={`Foto de ${nomeIntegrante}`}  className="lg:w-60 lg:h-60 w-48 h-48 rounded-full mb-5"></img>
                 <h2 className="text-xl">{nomeIntegrante}</h2>
                 <h3 className="">RM{rmIntegrante}</h3>
                 <h4 className="">{salaIntegrante}</h4>
-                <a className="" href={githubIntegrante}>{githubIntegrante}</a>
+                <a className="text-xs" href={githubIntegrante}>{githubIntegrante}</a>
             </div>
         </div>
         </>
--- FrontEnd/src/routes/Integrantes/index.tsx
+++ FrontEnd/src/routes/Integrantes/index.tsx
@@ -1,6 +1,13 @@
+import Cabecalho from "../../components/Cabecalho/Cabecalho";
+import BannerIntegrantes from "../../components/Integrantes/BannerIntegrantes";
+import IntegrantesGrupo from "../../components/Integrantes/IntegrantesGrupo";
+
 export default function Integrantes() {
   return (
     <>
+    <Cabecalho/>
+    <BannerIntegrantes/>
+    <IntegrantesGrupo/>
     </>
   )
 }
