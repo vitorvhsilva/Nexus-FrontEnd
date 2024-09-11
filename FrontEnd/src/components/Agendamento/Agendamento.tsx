3cea3c48cbaf1fcd6fd787970eb21bf6c3debf5a
joaogananca26 <joaogananca26@gmail.com>
2024-09-10 22:21:07 -0300

    Arrumando a estilização da página de agendamento

--- FrontEnd/src/components/Agendamento/Agendamento.tsx
+++ FrontEnd/src/components/Agendamento/Agendamento.tsx
@@ -1,11 +1,11 @@
 export default function Agendamento(){
     return(
         <section className="font-poppins">
-            <h1 className="text-2xl text-center my-5">Selecione a data para <span className="text-cor1">agendamento</span></h1>
+            <h1 className="text-2xl text-center my-10">Selecione a data para <span className="text-cor1">agendamento</span></h1>
             <form>
                 <div className="my-20">
                     <label className="text-xl mx-20">Data e hora</label>
-                    <input type="datetime-local" className="block border mx-20 rounded"/>
+                    <input type="datetime-local" className="block border mx-20 rounded mb-5"/>
                     <label className="text-xl mx-20 block">Escolha uma dessas mecânicas do seu bairro</label>
                     <select name="mecanicas" id="mecanicas" className="border rounded mx-20">
                     <option value="Jorge automóveis">Jorge automóveis</option>
