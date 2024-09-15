5ddf3b484f25c245199074cfcebcae958b85c9dd
vitorvhsilva <dndragonbr@gmail.com>
2024-09-15 19:39:27 -0300

    Script do Chatbot + Login com Header

--- FrontEnd/src/components/LoginECadastro/LoginComponent.tsx
+++ FrontEnd/src/components/LoginECadastro/LoginComponent.tsx
@@ -1,9 +1,18 @@
+import { Link } from "react-router-dom";
 import InputForm from "./InputForm";
 
 export default function LoginComponent() {
   return (
-    <main className="w-full h-[100vh] py-10 bg-cor1 flex justify-center items-center">
-      <form className="w-[95%] md:h-[95%] h-fit bg-corBranco rounded-2xl flex justify-around items-center md:flex-row flex-col">
+    <main className="w-full h-[100vh] py-10 bg-cor1 flex justify-center items-center flex-col">
+      <div className="w-[95%] h-[30%] bg-corBranco rounded-t-2xl flex justify-between px-5 items-center">
+        <Link to={"/"}>
+          <img src="./imgs/logos/nexusblack.png" alt="Logo da Nexus" className="w-24"/>
+        </Link> 
+        <Link to={"/cadastro"}>
+          <p className="text-lg font-poppins">Novo Usuário? <span className="text-cor2">Cadastre-se</span></p>
+        </Link>
+      </div>
+      <form className="w-[95%] md:h-[95%] h-fit bg-corBranco rounded-b-2xl flex justify-around items-center md:flex-row flex-col">
         <img src="./imgs/desenhos/bem-vindo.png" alt="Desenho de Boas Vindas" className="w-fit h-96"/>
         <div className="font-poppins">
           <h1 className="text-4xl text-center md:text-start">Bem vindo de volta!</h1>
@@ -11,7 +20,9 @@ export default function LoginComponent() {
           <InputForm placeholder="Insira seu email"/>
           <InputForm placeholder="Insira sua senha"/>
           <div className="flex justify-around items-center my-10 gap-8 md:gap-0 md:flex-row flex-col">
-            <button className="bg-cor2 text-corBranco text-2xl w-48 py-2 rounded-3xl">LOGIN</button>
+            <Link to="/user" className="navbar-link">
+              <button className="bg-cor2 text-corBranco text-2xl w-48 py-2 rounded-3xl">LOGIN</button>
+            </Link>
             <p>Esqueceu sua senha?</p>
           </div>
         </div>
--- FrontEnd/src/routes/Cadastro/index.tsx
+++ FrontEnd/src/routes/Cadastro/index.tsx
@@ -1,6 +1,7 @@
+import CadastroComponent from "../../components/LoginECadastro/CadastroComponent";
+
 export default function Cadastro() {
   return (
-    <>
-    </>
+    <CadastroComponent/>
   )
 }
