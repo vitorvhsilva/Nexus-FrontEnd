26029a1d29990ec62c11162f7f4ab5c704b702e3
vitorvhsilva <dndragonbr@gmail.com>
2024-10-24 20:42:39 -0300

    Cadastro de orcamentos feito

--- front-end/src/components/Orcamento/Orcamento.tsx
+++ front-end/src/components/Orcamento/Orcamento.tsx
@@ -22,7 +22,7 @@ export default function OrcamentoComponent() {
 
     const response = await fetch(`http://localhost:8080/veiculos/diagnostico/${cpf}`);
     const veiculosData = await response.json()
-
+    
     if (veiculosData.length == 0) {
       alert('Nenhum veiculo cadastrado!')
       navigate.push('/user/adicionar/veiculo')
@@ -47,6 +47,8 @@ export default function OrcamentoComponent() {
   const [diagnosticoSelecionado, setDiagnosticoSelecionado] = useState<TipoDiagnostico>({
     diagnosticoVeiculo: "", dataDiagnostico: [], feitoDiagnostico: 0, idVeiculo: 0, idDescricaoProblema: 0
   })
+
+  const [valorOrcamento, setValorOrcamento] = useState(null)
 
   const handleVeiculoClick = (veiculoSelecionado: TipoVeiculoUsuarioOrcamento) => {
     setVeiculoSelecionado(veiculoSelecionado);
@@ -84,8 +86,12 @@ export default function OrcamentoComponent() {
         return
       }
 
-      alert("Orçamento feito!")
-      navigate.push("/user")
+      const orcamento = await response.json();
+      console.log(orcamento)
+      setValorOrcamento(orcamento.valorOrcamento)
+
+      // alert(`Orçamento feito! Valor: ${valorOrcamento}`)
+      // navigate.push("/user")
 
     } catch (error) {
       alert(error)
@@ -96,16 +102,20 @@ export default function OrcamentoComponent() {
 
   return (
     <>
-    <main className="font-poppins">
+      <main className="font-poppins">
         <div className="w-[95%] h-fit grid xl:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-12 mx-auto rounded-xl my-10 p-5"> 
           {veiculos.map((v, i) => (
             <VeiculoUsuarioOrcamento key={i} marca={v.marca} modelo={v.modelo} ano={v.ano} placa={v.placa} tipo={v.tipo} diagnosticos={v.diagnosticos} selecionado={v.placa === veiculoSelecionado.placa} aoClicar={() => handleVeiculoClick(v)} aoClicarDiag={handleDiagnosticoClick} handleSubmit={handleSubmit}/>
           ))}
         </div>
 
-        <p className="text-xl text-center">Orçamento para o veículo: <span className="text-cor5">Toyota Corolla 2018 (XYZ-5421)</span> feito!</p>
-        <p className="text-2xl text-center mb-20">Valor: R$ XXX,XX</p>
-    </main>
-</>
+        {valorOrcamento != null && 
+          <>
+            <p className="text-xl text-center">Orçamento para o veículo: <span className="text-cor5">{veiculoSelecionado.marca + " " + veiculoSelecionado.modelo + " (" + veiculoSelecionado.placa + ")"} </span> feito!</p>
+            <p className="text-2xl text-center mb-20">Valor: {valorOrcamento}</p>
+          </>
+        } 
+      </main>
+    </>
   )
 }
